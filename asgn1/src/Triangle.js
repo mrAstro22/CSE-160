7a16faebfc21eb15069fca117445e2a51929e304
mrAstro22 <jaaalons@ucsc.edu>
2026-01-18 19:07:38 -0800

    done

---
+++ asgn1/src/Triangle.js
@@ -0,0 +1,50 @@
+class Triangle{
+  constructor(){
+    this.type = 'triangle';
+    this.postion = [0.0, 0.0, 0.0];
+    this.color = [1.0, 1.0, 1.0, 1.0];
+    this.size = 5.0; 
+  }
+
+  render() {
+    var xy = this.postion;
+    var rgba = this.color;
+    var size = 5.0;
+    
+    // Pass the color of a point to u_FragColor variable
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+    // Pass Size of point to u_Size variable
+    gl.uniform1f(u_Size, size);
+
+    // Draw
+    var d = this.size / 200.0; // deltaS
+    drawTriangle([xy[0], xy[1], xy[0] + d, xy[1], xy[0], xy[1] + d]);
+  }
+}
+
+function drawTriangle(vertices){
+    var n = 3; // The number of vertices
+    
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
+    // Bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // Write date into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    // Enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    // Draw the triangle
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+
+    return n;
+}
